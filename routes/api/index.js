530829a245cb55c46c3267d21b18aebeb3ef6c9e
Jacob H <jacob.hannah164@gmail.com>
2022-09-04 19:57:53 -0400

    added comments and reply functionality

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,6 @@ const commentRoutes = require('./comment-routes');
 
 
 router.use('/pizzas', pizzaRoutes);
-router.use('comments', commentRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
